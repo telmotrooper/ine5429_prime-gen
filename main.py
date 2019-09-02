b01f4aab2d397e157736d5e96b8d4f847ddeb847
Telmo "Trooper <telmo.trooper@gmail.com>
2019-09-02 11:31:42 -0300

    Generating 500 numbers

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from colors import cyan
 
 def main():
   print(cyan("Prime Number Generator"))
-  bbs = BlumBlumShub(11, 19, 3)
+  # bbs = BlumBlumShub(11, 19, 3)
+  # bbs = BlumBlumShub(5651, 5623, 53)
+  bbs = BlumBlumShub(30000000091, 40000000003, 7604055830)
 
-  for i in range(0,5):
+  for i in range(0,500):
     print(bbs.next())
 
 if __name__ == "__main__":
