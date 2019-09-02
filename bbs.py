c71ea00537dc50f4642f78f500ec7f4eb615d762
Telmo "Trooper <telmo.trooper@gmail.com>
2019-09-02 10:31:06 -0300

    Basic structure

---
+++ bbs.py
@@ -0,0 +1,4 @@
+# https://en.wikipedia.org/wiki/Blum_Blum_Shub
+
+def blum_blum_shub(p, q, s):
+  print(f"p = {p}, q = {q}, s = {s}")
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from bbs import blum_blum_shub
+
+def main():
+  print("Hello")
+  blum_blum_shub(11, 19, 3)
+
+if __name__ == "__main__":
+  main()
