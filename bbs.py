7d86185a9d8bfcd92641c22ec36fc77e3137ed9d
Telmo "Trooper <telmo.trooper@gmail.com>
2019-09-02 11:09:36 -0300

    BBS from function to class

--- bbs.py
+++ bbs.py
@@ -1,24 +1,32 @@
 from math import gcd
 from sys import exit
-from colors import red
+from colors import red, yellow
 
 # https://en.wikipedia.org/wiki/Blum_Blum_Shub
 
-def blum_blum_shub(p, q, s):
-  print(f"p = {p}, q = {q}, s = {s}")
-  check_seed(p,q,s)
+class BlumBlumShub:
+  def __init__(self, p, q, s):
+    print(yellow("Blum Blum Shub algorithm"))
+    print(f"p = {p}, q = {q}, s = {s}")
 
-def check_seed(p, q, s):
-  m = p * q
-  coprime = gcd(s, m) == 1
+    self.p = p
+    self.q = q
+    self.s = s
+    self.m = p * q
 
-  # The seed x0 should be an integer that is co-prime to M
-  # (i.e. p and q are not factors of x0) and not 1 or 0. 
-  if s == 0 or s == 1:
-    print(red("Error: 0 and 1 are not valid values for the seed."))
-    exit(1)
-  elif not coprime: 
-    print(red(f"Error: The seed {s} is not a coprime to m."))
-    exit(1)
-  else:
-    print(f"{s} is a valid seed.")
+    self.check_seed()
+
+  def check_seed(self):
+    coprime = gcd(self.s, self.m) == 1
+
+    # The seed x0 should be an integer that is co-prime to M
+    # (i.e. p and q are not factors of x0) and not 1 or 0. 
+    if self.s == 0 or self.s == 1:
+      print(red("Error: 0 and 1 are not valid values for the seed."))
+      exit(1)
+    elif not coprime: 
+      print(red(f"Error: The seed {self.s} is not a coprime to m."))
+      exit(1)
+
+  def next():
+    print("TODO")
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from bbs import blum_blum_shub
+from bbs import BlumBlumShub
 from colors import cyan
 
 def main():
   print(cyan("Prime Number Generator"))
-  blum_blum_shub(11, 19, 3)
+  bbs = BlumBlumShub(11, 19, 3)
 
 if __name__ == "__main__":
   main()
