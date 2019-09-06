c929c62bf01613d608d5a25ba900130d4506a5f1
Telmo "Trooper <telmo.trooper@gmail.com>
2019-09-05 23:16:37 -0300

    Changed p, q and x0 to very big values

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
+import sys
 from bbs import BlumBlumShub
 from colors import cyan
 
 def main():
   print(cyan("Prime Number Generator"))
-  # bbs = BlumBlumShub(11, 19, 3)
-  # bbs = BlumBlumShub(5651, 5623, 53)
-  bbs = BlumBlumShub(30000000091, 40000000003, 7604055830)
+
+  bbs = BlumBlumShub(
+    3000003453453450009130000034534534500091,
+    23432424245353535353452342343242424535353535345234,
+    23432424245353535353452341)
 
   for i in range(0,500):
-    print(bbs.next())
+    num = bbs.next()
+    print(f"{num} ({sys.getsizeof(num)} bytes)")
 
 if __name__ == "__main__":
   main()
