bb71eb22566a0d1ad55b209d8a3bcd82bd0dd0e7
Telmo "Trooper <telmo.trooper@gmail.com>
2019-09-09 11:41:53 -0300

    Working on Miller-Rabin

--- prime_checker.py
+++ prime_checker.py
@@ -1,2 +1,20 @@
+from random import randrange
+from colors import red
+
 def miller_rabin(num):
-  return(None)
+  # return(None)
+
+  s = 0
+  d = num-1
+
+  # write "n - 1" as "2^s * d"
+  while d % 2 == 0:
+    d >>= 1 # shift right 1 bit (divide by 2)
+    s += 1
+  
+  if not (pow(2,s) * d == num-1):
+    print(red(f"Error: Couldn't factor {num}."))
+    exit(1)
+
+  a = randrange(2, num-2)
+  # print(a)
